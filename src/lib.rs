50a8085336f512d560d559c5cd02f7ec6b796975
M4tthewDE <matthias.kronberg@gmail.com>
2023-12-04 19:45:45 +0100

    Implement addition

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,58 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{iter::zip, ops};
+
+#[derive(Debug)]
+pub struct Tensor {
+    pub data: Vec<i64>,
+    pub shape: Vec<usize>,
+}
+
+impl ops::Add<Tensor> for Tensor {
+    type Output = Tensor;
+
+    fn add(self, rhs: Tensor) -> Self::Output {
+        assert_eq!(self.shape, rhs.shape);
+
+        let result: Vec<i64> = zip(self.data, rhs.data).map(|(x1, x2)| x1 + x2).collect();
+
+        Tensor {
+            data: result,
+            shape: self.shape,
+        }
+    }
+}
+
+impl Tensor {
+    pub fn from_scalar(data: i64) -> Self {
+        Self {
+            data: vec![data],
+            shape: vec![1],
+        }
+    }
+
+    pub fn new(data: Vec<i64>, shape: Vec<usize>) -> Self {
+        Self { data, shape }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::Tensor;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn addition_1d() {
+        let a = Tensor::from_scalar(2);
+        let b = Tensor::from_scalar(3);
+        let result = a + b;
+
+        assert_eq!(result.data, vec![5]);
+    }
+
+    #[test]
+    fn addition_2d() {
+        let a = Tensor::new(vec![1, 2, 3, 4], vec![2, 2]);
+        let b = Tensor::new(vec![5, 6, 7, 8], vec![2, 2]);
+        let result = a + b;
+
+        assert_eq!(result.data, vec![6, 8, 10, 12]);
     }
 }
