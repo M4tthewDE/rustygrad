3c70bbe4d689660a3b6705c93a9492c11a980a40
M4tthewDE <matthias.kronberg@gmail.com>
2023-12-06 21:37:03 +0100

    Create global params

--- src/efficientnet.rs
+++ src/efficientnet.rs
@@ -1,6 +1,8 @@
 use crate::util;
 use anyhow::Result;
 
+// https://github.com/lukemelas/EfficientNet-PyTorch/blob/master/efficientnet_pytorch/model.py
+
 static MODEL_URLS: [&str; 8] = [
       "https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/efficientnet-b0-355c32eb.pth",
       "https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/efficientnet-b1-f1951068.pth",
@@ -12,14 +14,38 @@ static MODEL_URLS: [&str; 8] = [
       "https://github.com/lukemelas/EfficientNet-PyTorch/releases/download/1.0/efficientnet-b7-dcc49843.pth"
 ];
 
+static PARAMS: [(f64, f64, i64, f64); 8] = [
+    (1.0, 1.0, 224, 0.2),
+    (1.0, 1.1, 240, 0.2),
+    (1.1, 1.2, 260, 0.3),
+    (1.2, 1.4, 300, 0.3),
+    (1.4, 1.8, 380, 0.4),
+    (1.6, 2.2, 456, 0.4),
+    (1.8, 2.6, 528, 0.5),
+    (2.0, 3.1, 600, 0.5),
+];
+
+pub struct GlobalParams {
+    pub width_coefficient: f64,
+    pub depth_coefficient: f64,
+    pub image_size: i64,
+    pub dropout_rate: f64,
+    pub num_classes: usize,
+    pub batch_norm_momentum: f64,
+    pub batch_norm_epsilon: f64,
+    pub drop_connect_rate: f64,
+    pub depth_divisor: usize,
+    pub include_top: bool,
+}
+
 pub struct Efficientnet {
-    pub blocks_args: Vec<(f64, f64)>,
-    pub global_params: (f64, f64),
+    pub blocks_args: BlocksArgs,
+    pub global_params: GlobalParams,
 }
 
 impl Default for Efficientnet {
     fn default() -> Self {
-        let (blocks_args, global_params) = get_model_params(0).unwrap();
+        let (global_params, blocks_args) = get_model_params(0).unwrap();
 
         Self {
             blocks_args,
@@ -28,10 +54,28 @@ impl Default for Efficientnet {
     }
 }
 
-type GlobalParams = Vec<(f64, f64)>;
 type BlocksArgs = (f64, f64);
 
 fn get_model_params(number: usize) -> Result<(GlobalParams, BlocksArgs)> {
+    let _global_params = get_global_params(number);
     let _model_data = util::load_torch_model(MODEL_URLS[number])?;
-    todo!("extract blocks_args and global_params from json");
+
+    todo!("do whatever it takes");
+}
+
+fn get_global_params(number: usize) -> GlobalParams {
+    let (width, depth, res, dropout) = PARAMS[number];
+
+    GlobalParams {
+        width_coefficient: width,
+        depth_coefficient: depth,
+        image_size: res,
+        dropout_rate: dropout,
+        num_classes: 1000,
+        batch_norm_momentum: 0.99,
+        batch_norm_epsilon: 1e-3,
+        drop_connect_rate: 0.2,
+        depth_divisor: 8,
+        include_top: true,
+    }
 }
