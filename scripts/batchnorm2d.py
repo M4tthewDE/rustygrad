bb6f686a74420a733bd041c942eea07d680616d7
M4tthewDE <matthias.kronberg@gmail.com>
2023-12-22 12:18:21 +0100

    num_features 4 is enough for batchnorm2d test

--- scripts/batchnorm2d.py
+++ scripts/batchnorm2d.py
@@ -1,35 +1,32 @@
 import torch
 
 def run_batchnorm2d():
-    # FIXME: num_features_arr = [4, 8, 16, 32]
-    num_features_arr = [4]
-    for num_features in num_features_arr:
-        weight = torch.randn(num_features)
-        bias = torch.randn(num_features)
-        running_mean = torch.randn(num_features)
-        running_var = torch.randn(num_features)
+    num_features = 4
+    weight = torch.randn(num_features)
+    bias = torch.randn(num_features)
+    running_mean = torch.randn(num_features)
+    running_var = torch.randn(num_features)
 
-        print("WEIGHT", weight.detach().numpy())
-        print("BIAS", bias.detach().numpy())
-        print("RUNNING_MEAN", running_mean.detach().numpy())
-        print("RUNNING_VAR", running_var.detach().numpy())
+    print("WEIGHT", weight.detach().numpy())
+    print("BIAS", bias.detach().numpy())
+    print("RUNNING_MEAN", running_mean.detach().numpy())
+    print("RUNNING_VAR", running_var.detach().numpy())
 
-        with torch.no_grad():
-            bn = torch.nn.BatchNorm2d(num_features).eval()
-            bn.training = False
-            bn.weight[:] = weight
-            bn.bias[:] = bias
-            bn.running_mean[:] = running_mean
-            bn.running_var[:] = running_var 
-            # TODO: use random values
+    with torch.no_grad():
+        bn = torch.nn.BatchNorm2d(num_features).eval()
+        bn.training = False
+        bn.weight[:] = weight
+        bn.bias[:] = bias
+        bn.running_mean[:] = running_mean
+        bn.running_var[:] = running_var
 
-        inn = torch.rand([2, num_features, 3, 3])
-        print("INPUT")
-        print(inn.detach().numpy())
+    inn = torch.rand([2, num_features, 3, 3])
+    print("INPUT")
+    print(inn.detach().numpy())
 
-        out = bn(inn)
-        print("OUTPUT")
-        print(out.detach().numpy())
+    out = bn(inn)
+    print("OUTPUT")
+    print(out.detach().numpy())
 
 
 if __name__ == "__main__":
