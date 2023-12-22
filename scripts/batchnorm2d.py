ab772ad9a8c65f74119cfd0e0ac1254b8b1c3dff
M4tthewDE <matthias.kronberg@gmail.com>
2023-12-22 11:53:38 +0100

    Add batchnorm2d test case

---
+++ scripts/batchnorm2d.py
@@ -0,0 +1,36 @@
+import torch
+
+def run_batchnorm2d():
+    # FIXME: num_features_arr = [4, 8, 16, 32]
+    num_features_arr = [4]
+    for num_features in num_features_arr:
+        weight = torch.randn(num_features)
+        bias = torch.randn(num_features)
+        running_mean = torch.randn(num_features)
+        running_var = torch.randn(num_features)
+
+        print("WEIGHT", weight.detach().numpy())
+        print("BIAS", bias.detach().numpy())
+        print("RUNNING_MEAN", running_mean.detach().numpy())
+        print("RUNNING_VAR", running_var.detach().numpy())
+
+        with torch.no_grad():
+            bn = torch.nn.BatchNorm2d(num_features).eval()
+            bn.training = False
+            bn.weight[:] = weight
+            bn.bias[:] = bias
+            bn.running_mean[:] = running_mean
+            bn.running_var[:] = running_var 
+            # TODO: use random values
+
+        inn = torch.rand([2, num_features, 3, 3])
+        print("INPUT")
+        print(inn.detach().numpy())
+
+        out = bn(inn)
+        print("OUTPUT")
+        print(out.detach().numpy())
+
+
+if __name__ == "__main__":
+    run_batchnorm2d()
