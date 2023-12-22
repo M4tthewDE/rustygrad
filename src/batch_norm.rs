56a1f2bbe3153b0d2fb59af0509acf67d3c74880
M4tthewDE <matthias.kronberg@gmail.com>
2023-12-22 10:42:42 +0100

    Add builder for BatchNorm2d

--- src/batch_norm.rs
+++ src/batch_norm.rs
@@ -2,6 +2,7 @@ use crate::Tensor;
 
 const EXPO_AVG_FACTOR: f64 = 0.0;
 
+#[derive(Debug)]
 pub struct BatchNorm2d {
     running_mean: Tensor,
     running_var: Tensor,
@@ -9,36 +10,10 @@ pub struct BatchNorm2d {
     affine: bool,
     weight: Option<Tensor>,
     bias: Option<Tensor>,
-    _num_batches_tracked: usize,
 }
 
 // https://github.com/ptrblck/pytorch_misc/blob/master/batch_norm_manual.py
 impl BatchNorm2d {
-    pub fn new(num_features: usize, affine: Option<bool>) -> BatchNorm2d {
-        let (weight, bias) = if let Some(affine) = affine {
-            if affine {
-                (
-                    Some(Tensor::ones(num_features)),
-                    Some(Tensor::zeros(num_features)),
-                )
-            } else {
-                (None, None)
-            }
-        } else {
-            (None, None)
-        };
-
-        BatchNorm2d {
-            running_mean: Tensor::zeros(num_features),
-            running_var: Tensor::ones(num_features),
-            eps: 1e-5,
-            affine: true,
-            weight,
-            bias,
-            _num_batches_tracked: 0,
-        }
-    }
-
     pub fn forward(&mut self, input: Tensor, training: bool) -> Tensor {
         let mean: Tensor;
         let var: Tensor;
@@ -67,10 +42,61 @@ impl BatchNorm2d {
     }
 }
 
+pub struct BatchNorm2dBuilder {
+    num_features: usize,
+    eps: f64,
+    affine: bool,
+}
+
+impl BatchNorm2dBuilder {
+    pub fn new(num_features: usize) -> BatchNorm2dBuilder {
+        BatchNorm2dBuilder {
+            num_features,
+            eps: 1e-5,
+            affine: true,
+        }
+    }
+
+    pub fn eps(mut self, eps: f64) -> BatchNorm2dBuilder {
+        self.eps = eps;
+        self
+    }
+
+    pub fn affine(mut self, affine: bool) -> BatchNorm2dBuilder {
+        self.affine = affine;
+        self
+    }
+
+    pub fn build(self) -> BatchNorm2d {
+        let (weight, bias) = if self.affine {
+            (
+                Some(Tensor::ones(self.num_features)),
+                Some(Tensor::zeros(self.num_features)),
+            )
+        } else {
+            (None, None)
+        };
+
+        BatchNorm2d {
+            running_mean: Tensor::zeros(self.num_features),
+            running_var: Tensor::ones(self.num_features),
+            eps: self.eps,
+            affine: self.affine,
+            weight,
+            bias,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::batch_norm::BatchNorm2dBuilder;
+
     #[test]
     fn test_batchnorm2d() {
+        for num_features in vec![4, 8, 16, 32] {
+            let bn = BatchNorm2dBuilder::new(num_features).eps(1e-5).build();
+        }
         todo!("https://github.com/tinygrad/tinygrad/blob/38554322659fbe7e19c3cc7052465645274db5b9/test/test_nn.py#L25");
     }
 }
