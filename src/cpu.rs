294899fc12d2211199e06d5719acee5b54a4c37e
M4tthewDE <matthias.kronberg@gmail.com>
2024-01-05 16:14:05 +0100

    Do proper recursion

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,7 +1,31 @@
-use crate::{op::Op, tensor::Tensor};
+use std::iter::zip;
 
-impl Op {
-    pub fn run(&self) -> Tensor {
-        unimplemented!();
+use crate::{op::UnrealizedOp, tensor::Tensor};
+
+impl UnrealizedOp {
+    pub fn realize(&self) -> Tensor {
+        match self {
+            UnrealizedOp::Add(lhs, rhs) => {
+                let lhs = lhs.realize();
+                let rhs = rhs.realize();
+                let data: Vec<f64> = zip(lhs.data.unwrap(), rhs.data.unwrap())
+                    .map(|(l, r)| l + r)
+                    .collect();
+
+                Tensor::new(self.clone(), Some(data), Some(lhs.shape.unwrap().clone()))
+            }
+            UnrealizedOp::Sub(lhs, rhs) => {
+                let lhs = lhs.realize();
+                let rhs = rhs.realize();
+                let data: Vec<f64> = zip(lhs.data.unwrap(), rhs.data.unwrap())
+                    .map(|(l, r)| l - r)
+                    .collect();
+
+                Tensor::new(self.clone(), Some(data), Some(lhs.shape.unwrap().clone()))
+            }
+            UnrealizedOp::Load(data, shape) => {
+                Tensor::new(self.clone(), Some(data.clone()), Some(shape.clone()))
+            }
+        }
     }
 }
