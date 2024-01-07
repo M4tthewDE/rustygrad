9bfb07dcb7822f26e42b8950396b7cd0e0e00dd2
M4tthewDE <matthias.kronberg@gmail.com>
2024-01-07 14:56:56 +0100

    Back to the basics
    Mission remove clones

--- src/op.rs
+++ src/op.rs
@@ -6,28 +6,7 @@ use crate::tensor::Tensor;
 #[derive(Debug, Clone)]
 pub enum UnrealizedOp {
     Add(Box<Tensor>, Box<Tensor>),
-    Sub(Box<Tensor>, Box<Tensor>),
     Mul(Box<Tensor>, Box<Tensor>),
-    Div(Box<Tensor>, Box<Tensor>),
-    Max(Box<Tensor>),
-    Min(Box<Tensor>),
-    Sqrt(Box<Tensor>),
-    Log(Box<Tensor>),
-    Sigmoid(Box<Tensor>),
-    Relu(Box<Tensor>),
-    Sum(Box<Tensor>, Option<Vec<usize>>, bool),
-    Pool2D(Box<Tensor>, (usize, usize), usize, f64, PoolOp),
-    Conv2D(
-        Box<Tensor>,
-        Box<Tensor>,
-        Option<(usize, usize)>,
-        Option<usize>,
-    ),
-    Pad2D(Box<Tensor>, f64, [usize; 4]),
-    Reshape(Box<Tensor>, Vec<usize>),
-    Permute(Box<Tensor>, Vec<usize>),
-    MatMul(Box<Tensor>, Box<Tensor>),
-    Expand(Box<Tensor>, Vec<usize>),
     Load(Vec<f64>, Vec<usize>),
 }
 
