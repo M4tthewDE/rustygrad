ea3a4350f6735612cb902dfe7fbd1a69369e21bf
M4tthewDE <matthias.kronberg@gmail.com>
2024-01-08 21:25:04 +0100

    Simplify loop detection

--- src/loops.rs
+++ src/loops.rs
@@ -12,43 +12,41 @@ pub fn is_loop(t: &Tensor) -> bool {
     is_cyclical(&mut seen_ops, &t.unrealized_op)
 }
 
-fn see_op(seen_ops: &mut HashSet<usize>, id: usize, next_ops: Vec<&UnrealizedOp>) -> bool {
+fn is_cyclical(seen_ops: &mut HashSet<usize>, op: &UnrealizedOp) -> bool {
+    let (id, children) = match op {
+        UnrealizedOp::Add(lhs, rhs, id) => (*id, vec![lhs, rhs]),
+        UnrealizedOp::Sub(lhs, rhs, id) => (*id, vec![lhs, rhs]),
+        UnrealizedOp::Mul(lhs, rhs, id) => (*id, vec![lhs, rhs]),
+        UnrealizedOp::Div(lhs, rhs, id) => (*id, vec![lhs, rhs]),
+        UnrealizedOp::Max(t, id) => (*id, vec![t]),
+        UnrealizedOp::Min(t, id) => (*id, vec![t]),
+        UnrealizedOp::Sqrt(t, id) => (*id, vec![t]),
+        UnrealizedOp::Log(t, id) => (*id, vec![t]),
+        UnrealizedOp::Load(_, _, id) => (*id, vec![]),
+        UnrealizedOp::Sigmoid(t, id) => (*id, vec![t]),
+        UnrealizedOp::Relu(t, id) => (*id, vec![t]),
+        UnrealizedOp::Sum(t, _, _, id) => (*id, vec![t]),
+        UnrealizedOp::Pool2D(t, _, _, _, _, id) => (*id, vec![t]),
+        UnrealizedOp::Conv2D(t, kernel, _, _, id) => (*id, vec![t, kernel]),
+        UnrealizedOp::Pad2D(t, _, _, id) => (*id, vec![t]),
+        UnrealizedOp::Reshape(t, _, id) => (*id, vec![t]),
+        UnrealizedOp::Permute(t, _, id) => (*id, vec![t]),
+        UnrealizedOp::Expand(t, _, id) => (*id, vec![t]),
+        UnrealizedOp::MatMul(lhs, rhs, id) => (*id, vec![lhs, rhs]),
+    };
+
     if seen_ops.contains(&id) {
         return true;
     }
 
     seen_ops.insert(id);
 
-    for op in next_ops {
-        if is_cyclical(seen_ops, op) {
-            debug!("{:?}, {}", op, id);
+    for child in children {
+        if is_cyclical(seen_ops, child) {
+            debug!("{:?}, {}", child, id);
             return true;
         }
     }
 
     false
-}
-
-fn is_cyclical(seen_ops: &mut HashSet<usize>, op: &UnrealizedOp) -> bool {
-    match op {
-        UnrealizedOp::Add(lhs, rhs, id) => see_op(seen_ops, *id, vec![lhs, rhs]),
-        UnrealizedOp::Sub(lhs, rhs, id) => see_op(seen_ops, *id, vec![lhs, rhs]),
-        UnrealizedOp::Mul(lhs, rhs, id) => see_op(seen_ops, *id, vec![lhs, rhs]),
-        UnrealizedOp::Div(lhs, rhs, id) => see_op(seen_ops, *id, vec![lhs, rhs]),
-        UnrealizedOp::Max(t, id) => see_op(seen_ops, *id, vec![t]),
-        UnrealizedOp::Min(t, id) => see_op(seen_ops, *id, vec![t]),
-        UnrealizedOp::Sqrt(t, id) => see_op(seen_ops, *id, vec![t]),
-        UnrealizedOp::Log(t, id) => see_op(seen_ops, *id, vec![t]),
-        UnrealizedOp::Load(_, _, id) => see_op(seen_ops, *id, vec![]),
-        UnrealizedOp::Sigmoid(t, id) => see_op(seen_ops, *id, vec![t]),
-        UnrealizedOp::Relu(t, id) => see_op(seen_ops, *id, vec![t]),
-        UnrealizedOp::Sum(t, _, _, id) => see_op(seen_ops, *id, vec![t]),
-        UnrealizedOp::Pool2D(t, _, _, _, _, id) => see_op(seen_ops, *id, vec![t]),
-        UnrealizedOp::Conv2D(t, kernel, _, _, id) => see_op(seen_ops, *id, vec![t, kernel]),
-        UnrealizedOp::Pad2D(t, _, _, id) => see_op(seen_ops, *id, vec![t]),
-        UnrealizedOp::Reshape(t, _, id) => see_op(seen_ops, *id, vec![t]),
-        UnrealizedOp::Permute(t, _, id) => see_op(seen_ops, *id, vec![t]),
-        UnrealizedOp::Expand(t, _, id) => see_op(seen_ops, *id, vec![t]),
-        UnrealizedOp::MatMul(lhs, rhs, id) => see_op(seen_ops, *id, vec![lhs, rhs]),
-    }
 }
