94bd25fa5cd2d73450b002b175c1586105d6a7a5
M4tthewDE <matthias.kronberg@gmail.com>
2024-01-13 16:06:56 +0100

    Simplify mul

--- src/tensor.rs
+++ src/tensor.rs
@@ -380,26 +380,16 @@ impl ops::Mul<f64> for Tensor {
     type Output = Tensor;
 
     fn mul(self, rhs: f64) -> Self::Output {
-        let (l, r, shape) = broadcast_shapes(&self.shape, &[1]);
-        let lhs = self.reshape(l).expand(shape.clone());
-        let rhs = Tensor::from_scalar(rhs).reshape(r).expand(shape.clone());
-        Tensor::from_op(
-            Op::Mul(Rc::new(lhs.unrealized_op), Rc::new(rhs.unrealized_op)),
-            &shape,
-        )
+        let rhs = Tensor::from_scalar(rhs);
+        self * rhs
     }
 }
 
 impl ops::Mul<Tensor> for f64 {
     type Output = Tensor;
     fn mul(self, rhs: Tensor) -> Self::Output {
-        let (l, r, shape) = broadcast_shapes(&[1], &rhs.shape);
-        let lhs = Tensor::from_scalar(self).reshape(l).expand(shape.clone());
-        let rhs = rhs.reshape(r).expand(shape.clone());
-        Tensor::from_op(
-            Op::Mul(Rc::new(lhs.unrealized_op), Rc::new(rhs.unrealized_op)),
-            &shape,
-        )
+        let lhs = Tensor::from_scalar(self);
+        lhs * rhs
     }
 }
 
@@ -408,8 +398,16 @@ impl ops::Mul<Tensor> for Tensor {
 
     fn mul(self, rhs: Tensor) -> Self::Output {
         let (l, r, shape) = broadcast_shapes(&self.shape, &rhs.shape);
-        let lhs = self.reshape(l).expand(shape.clone());
-        let rhs = rhs.reshape(r).expand(shape.clone());
+        let lhs = if l != shape {
+            self.reshape(l).expand(shape.clone())
+        } else {
+            self
+        };
+        let rhs = if r != shape {
+            rhs.reshape(r).expand(shape.clone())
+        } else {
+            rhs
+        };
 
         Tensor::from_op(
             Op::Mul(Rc::new(lhs.unrealized_op), Rc::new(rhs.unrealized_op)),
