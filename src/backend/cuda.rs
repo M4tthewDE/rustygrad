b96368fe071f70183e84fbd06f047ce5e4267618
M4tthewDE <matthias.kronberg@gmail.com>
2024-01-13 15:18:01 +0100

    Load from GPU to host

--- src/backend/cuda.rs
+++ src/backend/cuda.rs
@@ -1,4 +1,4 @@
-use std::ffi::{c_int, c_void};
+use std::ffi::{c_int, c_void, CStr};
 
 use tracing::trace;
 
@@ -8,13 +8,13 @@ extern "C" {
     fn cudaMalloc(devPtr: *mut *mut c_void, size: usize) -> c_int;
     fn cudaMemcpy(dst: *mut c_void, src: *const c_void, count: usize, kind: c_int) -> c_int;
     fn cudaFree(devPtr: *mut c_void) -> c_int;
+    fn cudaGetErrorString(error: c_int) -> *const i8;
 }
 
 const HOST_TO_DEVICE: c_int = 1;
-const _DEVICE_TO_HOST: c_int = 2;
+const DEVICE_TO_HOST: c_int = 2;
 
-pub fn realize(op: &Op) -> (Vec<f64>, Vec<usize>) {
-    trace!("Realizing {:?}", op);
+pub fn realize_cuda(op: &Op, mut dev_ptr: *mut c_void) -> (Vec<usize>, *mut c_void) {
     match op {
         Op::Add(_, _) => todo!(),
         Op::Sub(_, _) => todo!(),
@@ -24,15 +24,13 @@ pub fn realize(op: &Op) -> (Vec<f64>, Vec<usize>) {
         Op::Min(_) => todo!(),
         Op::Sqrt(_) => todo!(),
         Op::Log(_) => todo!(),
-        Op::Load(data, _) => {
-            let mut dev_ptr: *mut c_void = std::ptr::null_mut();
+        Op::Load(data, shape) => {
             let bytes = data.len() * std::mem::size_of::<f64>();
 
             unsafe {
                 let result = cudaMalloc(&mut dev_ptr as *mut *mut c_void, bytes);
                 assert_eq!(result, 0, "cudaMalloc failled with error code {}", result);
 
-                // Copy data from the host vector to the GPU
                 let result = cudaMemcpy(
                     dev_ptr,
                     data.as_ptr() as *const c_void,
@@ -44,7 +42,7 @@ pub fn realize(op: &Op) -> (Vec<f64>, Vec<usize>) {
                     panic!("Failed to copy data to the GPU {}", result);
                 }
             }
-            todo!();
+            (shape.to_vec(), dev_ptr)
         }
         Op::Sigmoid(_) => todo!(),
         Op::Relu(_) => todo!(),
@@ -57,4 +55,31 @@ pub fn realize(op: &Op) -> (Vec<f64>, Vec<usize>) {
         Op::Expand(_, _) => todo!(),
         Op::MatMul(_, _) => todo!(),
     }
+}
+
+pub fn realize(op: &Op) -> (Vec<f64>, Vec<usize>) {
+    trace!("Realizing {:?}", op);
+
+    let dev_ptr: *mut c_void = std::ptr::null_mut();
+
+    let (shape, dev_ptr) = realize_cuda(op, dev_ptr);
+
+    let result_size = shape.iter().product();
+    let mut result = vec![0.0; result_size];
+    unsafe {
+        let code = cudaMemcpy(
+            result.as_mut_ptr() as *mut c_void,
+            dev_ptr,
+            result_size * std::mem::size_of::<f64>(),
+            DEVICE_TO_HOST,
+        );
+        if code != 0 {
+            let error_str = CStr::from_ptr(cudaGetErrorString(code))
+                .to_string_lossy()
+                .into_owned();
+            panic!("Failed to copy data from the GPU to host: {}", error_str);
+        }
+    }
+
+    (result, shape)
 }
