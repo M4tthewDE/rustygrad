c82d00702f48d49844a60d2722caf087215ba8b0
M4tthewDE <matthias.kronberg@gmail.com>
2024-01-13 11:10:04 +0100

    Add conv2d benchmark

---
+++ benches/conv2d.rs
@@ -0,0 +1,17 @@
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use rustygrad::tensor::Tensor;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    std::env::set_var("NO_CACHE", "1");
+
+    let t = Tensor::rand(vec![1, 3, 100, 100]);
+    let kernel = Tensor::rand(vec![1, 3, 10, 10]);
+    let t = t.conv2d(kernel, None, None, None, None);
+
+    c.bench_function("conv2d", move |b| {
+        b.iter_batched(|| t.clone(), |t| t.realize(), BatchSize::SmallInput)
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
