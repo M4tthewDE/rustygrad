50789f86b55ff5e278da791baafa2aa49440231b
M4tthewDE <matthias.kronberg@gmail.com>
2024-01-14 20:33:29 +0100

    Rename add lib to generic backend lib

--- build.rs
+++ build.rs
@@ -6,13 +6,13 @@ fn main() {
         .flag("-cudart=shared")
         .flag("-gencode")
         .flag("arch=compute_75,code=sm_75")
-        .file("resources/add.cu")
-        .compile("libadd.a");
+        .file("resources/backend.cu")
+        .compile("libbackend.a");
 
     // Path to the CUDA runtime library
     let cuda_lib_dir = "/usr/local/cuda/lib64";
 
-    println!("cargo:rerun-if-changed=resources/add.cu");
+    println!("cargo:rerun-if-changed=resources/backend.cu");
     println!("cargo:rustc-link-search=native={}", cuda_lib_dir);
     println!("cargo:rustc-link-lib=cudart");
 }
