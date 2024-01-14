9ed97d453cb63cce905636357f65842e593156a3
M4tthewDE <matthias.kronberg@gmail.com>
2024-01-14 20:31:11 +0100

    Create functions for cuda access

--- src/backend/cuda.rs
+++ src/backend/cuda.rs
@@ -4,9 +4,13 @@ use tracing::trace;
 
 use crate::op::Op;
 
+const HOST_TO_DEVICE: c_int = 1;
+const DEVICE_TO_HOST: c_int = 2;
+
 extern "C" {
     fn cudaMalloc(devPtr: *mut *mut c_void, size: usize) -> c_int;
     fn cudaMemcpy(dst: *mut c_void, src: *const c_void, count: usize, kind: c_int) -> c_int;
+    // FIXME: we should really use this at some point...
     //fn cudaFree(devPtr: *mut c_void) -> c_int;
     fn cudaGetErrorString(error: c_int) -> *const i8;
     fn cudaGetLastError() -> c_int;
@@ -14,30 +18,65 @@ extern "C" {
     fn add(a: *const c_void, b: *const c_void, c: *mut c_void, n: usize);
 }
 
-const HOST_TO_DEVICE: c_int = 1;
-const DEVICE_TO_HOST: c_int = 2;
+unsafe fn error_string(code: i32) -> String {
+    CStr::from_ptr(cudaGetErrorString(code))
+        .to_string_lossy()
+        .into_owned()
+}
+
+unsafe fn check_last_error() {
+    let code = cudaGetLastError();
+    if code != 0 {
+        panic!("{}", error_string(code));
+    }
+}
+
+unsafe fn malloc(size: usize) -> *mut c_void {
+    let mut ptr: *mut c_void = std::ptr::null_mut();
+    let code = cudaMalloc(
+        &mut ptr as *mut *mut c_void,
+        size * std::mem::size_of::<f64>(),
+    );
+    if code != 0 {
+        panic!("{}", error_string(code));
+    }
+
+    ptr
+}
+
+unsafe fn memcpy_to_device(ptr: *mut c_void, data: &Vec<f64>) {
+    let code = cudaMemcpy(
+        ptr,
+        data.as_ptr() as *const c_void,
+        data.len() * std::mem::size_of::<f64>(),
+        HOST_TO_DEVICE,
+    );
+    if code != 0 {
+        panic!("{}", error_string(code));
+    }
+}
+
+unsafe fn memcpy_to_host(result: &mut Vec<f64>, ptr: *mut c_void, size: usize) {
+    let code = cudaMemcpy(
+        result.as_mut_ptr() as *mut c_void,
+        ptr,
+        size * std::mem::size_of::<f64>(),
+        DEVICE_TO_HOST,
+    );
+    if code != 0 {
+        panic!("{}", error_string(code));
+    }
+}
 
 unsafe fn realize_cuda(op: &Op) -> (*mut c_void, Vec<usize>) {
     match op {
         Op::Add(lhs, rhs) => {
             let (lhs_ptr, shape) = realize_cuda(&lhs.op);
             let (rhs_ptr, _) = realize_cuda(&rhs.op);
-
             let result_size = shape.iter().product::<usize>();
-            let mut result_ptr: *mut c_void = std::ptr::null_mut();
-            let code = cudaMalloc(
-                &mut result_ptr as *mut *mut c_void,
-                result_size * std::mem::size_of::<f64>(),
-            );
-            if code != 0 {
-                panic!("{}", error_string(code));
-            }
-
+            let result_ptr = malloc(result_size);
             add(lhs_ptr, rhs_ptr, result_ptr, result_size);
-            let code = cudaGetLastError();
-            if code != 0 {
-                panic!("{}", error_string(code));
-            }
+            check_last_error();
             (result_ptr, shape)
         }
         Op::Sub(_, _) => todo!(),
@@ -48,21 +87,8 @@ unsafe fn realize_cuda(op: &Op) -> (*mut c_void, Vec<usize>) {
         Op::Sqrt(_) => todo!(),
         Op::Log(_) => todo!(),
         Op::Load(data, shape) => {
-            let bytes = data.len() * std::mem::size_of::<f64>();
-            let mut dev_ptr: *mut c_void = std::ptr::null_mut();
-            let code = cudaMalloc(&mut dev_ptr as *mut *mut c_void, bytes);
-            if code != 0 {
-                panic!("{}", error_string(code));
-            }
-            let code = cudaMemcpy(
-                dev_ptr,
-                data.as_ptr() as *const c_void,
-                bytes,
-                HOST_TO_DEVICE,
-            );
-            if code != 0 {
-                panic!("{}", error_string(code));
-            }
+            let dev_ptr = malloc(data.len());
+            memcpy_to_device(dev_ptr, data);
             (dev_ptr, shape.to_vec())
         }
         Op::Sigmoid(_) => todo!(),
@@ -91,22 +117,7 @@ pub fn realize(op: &Op) -> (Vec<f64>, Vec<usize>) {
         let (result_ptr, shape) = realize_cuda(op);
         let result_size = shape.iter().product();
         let mut result = vec![0.0; result_size];
-        let code = cudaMemcpy(
-            result.as_mut_ptr() as *mut c_void,
-            result_ptr,
-            result_size * std::mem::size_of::<f64>(),
-            DEVICE_TO_HOST,
-        );
-        if code != 0 {
-            panic!("{}", error_string(code));
-        }
-
+        memcpy_to_host(&mut result, result_ptr, result_size);
         (result, shape)
     }
-}
-
-unsafe fn error_string(code: i32) -> String {
-    CStr::from_ptr(cudaGetErrorString(code))
-        .to_string_lossy()
-        .into_owned()
 }
