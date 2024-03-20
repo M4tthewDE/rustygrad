f3a3e99c1b2ee6168621b79dac9f1423ddb3f919
M4tthewDE <matthias.kronberg@gmail.com>
2024-03-20 21:50:14 +0100

    Add CUDA environment variable

--- src/device.rs
+++ src/device.rs
@@ -3,13 +3,23 @@ use std::sync::Mutex;
 use lazy_static::lazy_static;
 
 lazy_static! {
-    static ref DEVICE: Mutex<Device> = Mutex::new(Device::Cpu);
+    static ref DEVICE: Mutex<Device> = Mutex::new(Device::from_env());
 }
 
 #[derive(Clone)]
 pub enum Device {
     Cpu,
     Cuda,
+}
+
+impl Device {
+    fn from_env() -> Device {
+        if std::env::var("CUDA").is_ok() {
+            Device::Cuda
+        } else {
+            Device::Cpu
+        }
+    }
 }
 
 pub fn get_device() -> Device {
